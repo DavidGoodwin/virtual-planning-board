56d4d952c3e2258385fa8181ef4e1cbbf3d993bb
Tom Mortimer-Jones <tom@morty.co.uk>
2010-02-23 17:31:23 +0000

    Modified form so that admin interface presents user with a list of days instead of requiring them to enter a number for the required day.

--- board/forms.py
+++ board/forms.py
@@ -1,6 +1,7 @@
 from board.models import Person, WeekDayPersonHours
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.forms import ModelForm
+from django.forms import ModelForm, ChoiceField
+import calendar
 
 class PersonCreationForm(UserCreationForm):
 
@@ -14,6 +15,7 @@ class PersonChangeForm(UserChangeForm):
         model = Person
 
 class WeekDayPersonHoursAssignForm(ModelForm):
+    day = ChoiceField(choices=[(i, calendar.day_name[i]) for i in range(5)])
 
     class Meta:
         model = WeekDayPersonHours
